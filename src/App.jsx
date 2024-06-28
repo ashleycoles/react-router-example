297358d51c09b024647046090c6f31de4596d873
Ash Coles <ashley@acoles.co.uk>
2024-06-28 09:54:09 +0100

    Working routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Footer from "./components/Footer"
 import Nav from "./components/Nav"
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+import HomePage from "./pages/HomePage"
 
 function App() {
-  return (
-    <>
-      <Nav />
+    return (
+        <BrowserRouter>
 
-      <Footer />
-    </>
-  )
+            <Nav />
+
+            {/* This is the swapable window */}
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+            </Routes>
+
+            <Footer />
+
+        </BrowserRouter>
+    )
 }
 
 export default App
--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 export default function Nav() {
     return (
         <nav>
-            <a href="#">Home</a>
-            <a href="#">About</a>
-            <a href="#">Contact</a>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+            <Link to='/contact'>Contact</Link>
         </nav>
     )
 }
